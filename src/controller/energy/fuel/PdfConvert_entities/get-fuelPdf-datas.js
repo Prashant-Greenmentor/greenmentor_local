0c5d6f1a1a27eeaa32df747227ca84cbf82122ea
Prashant Rathour <prathour884@gmail.com>
2024-03-12 19:17:49 +0530

    update change html to puppeteer -

--- src/controller/energy/fuel/PdfConvert_entities/get-fuelPdf-datas.js
+++ src/controller/energy/fuel/PdfConvert_entities/get-fuelPdf-datas.js
@@ -128,7 +128,7 @@ const fuelDataGetPdf = ({ selectFuelData }) => {
         return Formatedata[key.trim()] || '';
       });
 
-      const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({headless: false});
       const page = await browser.newPage();
       await page.setContent(filledHtml);
 
